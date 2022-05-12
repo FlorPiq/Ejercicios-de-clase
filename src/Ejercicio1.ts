d2ad786a6f15a521c43a28618b493c7433d95c0b
FlorPiq <104107420+FlorPiq@users.noreply.github.com>
2022-05-12 02:56:35 -0300

    Initial commit

---
+++ src/Ejercicio1.ts
@@ -0,0 +1,7 @@
+console.log("Pasos para un posteo en Facebook");
+
+console.log("Paso 1: Escribir un texto");
+console.log("Paso 2: Subir una foto");
+console.log("Paso 3: Agregarle musica");
+console.log("Paso 4: clickear en boton publicar");
+console.log("Paso 5: Compartir posteo");
